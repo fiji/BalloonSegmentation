f2c35e40af725ffe500c50240a6f72b07916dc86
Curtis Rueden <ctrueden@wisc.edu>
2017-02-23 10:07:39 -0600

    Add license blurbs and project license
    Accomplished using:
        mvn license:update-file-header license:update-project-license
    
    Along with some manual tweaking.

--- src/main/java/balloonstructure/BalloonPopulation.java
+++ src/main/java/balloonstructure/BalloonPopulation.java
@@ -1,3 +1,24 @@
+/*-
+ * #%L
+ * Balloon Segmentation plugin for Fiji.
+ * %%
+ * Copyright (C) 2012 - 2017 Fiji development team.
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
 package balloonstructure;
 /*********************************************************************
  * Version: February, 2008
@@ -20,21 +41,6 @@ package balloonstructure;
  * Lionel.Dupuy@scri.ac.uk
  * http://www.scri.ac.uk/staff/lioneldupuy
  ********************************************************************/
-/**
-	This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License along
-with this program; if not, write to the Free Software Foundation, Inc.,
-51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA."""
-*/
 
 import Jama.Matrix;
 import ij.IJ;
